00c119d5cf2d4df8360432c43e94c28e15562cbb
Chris Nosowsky <ChrisNosowsky@gmail.com>
2019-02-08 16:02:35 -0500

    Create hello.py

---
+++ Projects/Project0/hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import getpass
+import sys
+
+print('hello, world')
+print('I am {0}'.format(getpass.getuser()))
+print('I am running Python {0}'.format(sys.version))
+print('My name is Chris Nosowsky and I have read the syllabus and understand the penalties for academic dishonesty.')
